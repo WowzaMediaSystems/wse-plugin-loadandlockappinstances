445f1ca0b0675528e5f4aeab4fa701ae7d7a4eb2
Matt Young <myoung@wowza.com>
2016-09-08 04:45:51 -0400

    initial commit of load and lock app instances server listener.

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# wse-plugin-loadandlockappinstances
+# Load And Lock App Instances 
+The **LoadAndLockAppInstances** server listener for [Wowza Streaming Engine™ media server software](https://www.wowza.com/products/streaming-engine) allows you to have an application load and remain loaded until the Streaming Engine is shut down.
+
+## Prerequisites
+Wowza Streaming Engine 4.0.0 or later is required.
+
+## Usage
+Specify the application(s) per vhost of which to load by leveraging the appropriate properties.
+
+## More resources
+[Wowza Streaming Engine Server-Side API Reference](https://www.wowza.com/resources/WowzaStreamingEngine_ServerSideAPI.pdf)
+
+[How to extend Wowza Streaming Engine using the Wowza IDE](https://www.wowza.com/forums/content.php?759-How-to-extend-Wowza-Streaming-Engine-using-the-Wowza-IDE)
+
+Wowza Media Systems™ provides developers with a platform to create streaming applications and solutions. See [Wowza Developer Tools](https://www.wowza.com/resources/developers) to learn more about our APIs and SDK.
+
+To use the compiled version of this module, see [How to use a server listener to load and lock an appinstance](https://www.wowza.com/forums/content.php?155-How-to-use-a-server-listener-to-load-and-lock-an-appinstance-(LoadAndLockAppInstances)).
+
+## Contact
+[Wowza Media Systems, LLC](https://www.wowza.com/contact)
+
+## License
+This code is distributed under the [Wowza Public License](https://github.com/WowzaMediaSystems/wse-plugin-loadandlockinstances/blob/master/LICENSE.txt).
+
+![alt tag](http://wowzalogs.com/stats/githubimage.php?plugin=wse-plugin-loadandlockinstances)
---
+++ src/com/wowza/wms/plugin/LoadAndLockAppInstances.java
@@ -0,0 +1,75 @@
+/**
+ * Wowza server software and all components Copyright 2006 - 2015, Wowza Media Systems, LLC, licensed pursuant to the Wowza Media Software End User License Agreement.
+ */
+package com.wowza.wms.plugin;
+
+import com.wowza.util.StringUtils;
+import com.wowza.wms.application.IApplicationInstance;
+import com.wowza.wms.application.WMSProperties;
+import com.wowza.wms.logging.*;
+import com.wowza.wms.server.*;
+import com.wowza.wms.vhost.IVHost;
+import com.wowza.wms.vhost.VHostSingleton;
+
+public class LoadAndLockAppInstances implements IServerNotify2 {
+
+	public static final String CLASS_NAME = "LoadAndLockAppInstances";
+	public static final String PROP_NAME_PREFIX = "loadAndLock";
+	
+	private String vhostName = IVHost.VHOST_DEFAULT;
+	private String appNames = ""; // live/_definst_
+
+	public void onServerConfigLoaded(IServer server) {
+	}
+
+	public void onServerCreate(IServer server) {
+	}
+
+	public void onServerInit(IServer server) {
+		WMSLoggerFactory.getLogger(null).info(CLASS_NAME + ".onServerInit");
+		WMSProperties props = server.getProperties();
+		this.vhostName  = props.getPropertyStr(PROP_NAME_PREFIX + "VHost", this.vhostName);
+		this.appNames  = props.getPropertyStr(PROP_NAME_PREFIX + "Applications", this.appNames);
+		
+		if(!StringUtils.isEmpty(this.appNames)){
+			String[] contexts = this.appNames.split(",");
+			for (String context : contexts)
+			{
+				String[] parts = context.trim().split("/");
+				String appName = parts[0].trim();
+				String appInstName = IApplicationInstance.DEFAULT_APPINSTANCE_NAME;
+				if (parts.length > 1)
+				{
+					appInstName = parts[1].trim();
+				}
+				if (!StringUtils.isEmpty(vhostName) && !StringUtils.isEmpty(appName) && !StringUtils.isEmpty(appInstName))
+				{
+					loadAndLockAppInstance(vhostName, appName, appInstName);
+				}
+			}
+		}
+	}
+
+	public void onServerShutdownStart(IServer server) {
+	}
+
+	public void onServerShutdownComplete(IServer server) {
+	}
+	
+	private void loadAndLockAppInstance(String vhostName, String appName, String appInstanceName)
+	{
+		IVHost vhost = VHostSingleton.getInstance(vhostName);
+		if(vhost != null)
+		{
+			if (vhost.startApplicationInstance(appName, appInstanceName))
+			{
+				vhost.getApplication(appName).getAppInstance(appInstanceName).setApplicationTimeout(0);
+			}
+			else
+			{
+				WMSLoggerFactory.getLoggerObj(vhost).warn(CLASS_NAME + ".loadAndLockAppInstance: Application folder ([install-location]/applications/" + appName + ") is missing", WMSLoggerIDs.CAT_vhost, WMSLoggerIDs.EVT_comment);
+			}
+		}
+	}
+
+}
